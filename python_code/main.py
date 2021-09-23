2bb4c46ed730e3ba5c392cb5ccebf33591cc737c
tzwane <thembinkosi.zwane@gmail.com>
2021-09-23 03:10:54 +0200

    Import and add Thembinkosi's greeting

--- python_code/main.py
+++ python_code/main.py
@@ -1,5 +1,13 @@
-def main():
-    print("Welcome to our collaborative program.")
+import zwane
 
+# Main starts the program
+def main():
+    # Print welcome message
+    print("Welcome to our collaborative program.")
+    print()
+    # Print  greetings
+    zwane.display_greeting()
+
+# Start main
 if __name__ == "__main__":
     main()
